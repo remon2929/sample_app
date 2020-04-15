cac2d5ad7fb113362d9aa4ec18173be3f9eca65e
remon2929 <ff.remon.9123@gmail.com>
2020-04-15 11:44:51 +0000

    リスト 6.39: パスワードとパスワード確認を追加する

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -54,5 +55,6 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+  
 
 end
