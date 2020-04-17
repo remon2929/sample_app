ae2e66437b8a5afdb29d385a264fcc2f2ce86849
remon2929 <ff.remon.9123@gmail.com>
2020-04-17 06:28:53 +0000

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,10 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+ # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
