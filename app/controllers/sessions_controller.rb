85f23a8641f2973f3f783834227e03adb00a2034
remon2929 <ff.remon.9123@gmail.com>
2020-04-18 10:40:22 +0000

    8.15

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,17 @@ user = User.find_by(email: params[:session][:email].downcase)
       render 'new'
     end
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 
   def destroy
   end
