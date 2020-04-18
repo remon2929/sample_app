4412e2bf008778d4301af8ced1185d97605fbd41
remon2929 <ff.remon.9123@gmail.com>
2020-04-18 10:49:38 +0000

    8.19

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,6 +25,13 @@ user = User.find_by(email: params[:session][:email].downcase)
       render 'new'
     end
   end
+  
+   # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 
   def destroy
   end
