f248dbe809cd028eda89d2069747e5de5c719000
remon2929 <ff.remon.9123@gmail.com>
2020-04-21 04:07:11 +0000

    10.26

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 before_action :logged_in_user, only: [:edit, :update]
-
+ before_action :correct_user,   only: [:edit, :update]
 def show
     @user = User.find(params[:id])
   
@@ -13,7 +13,7 @@ end
 
 
 def edit
-    @user = User.find(params[:id])
+
 end
 
 
@@ -34,7 +34,7 @@ end
    end
    
    def update
-    @user = User.find(params[:id])
+   
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -56,6 +56,12 @@ end
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+    
+     # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 
 end
