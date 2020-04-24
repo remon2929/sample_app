92bc9c12ec1a8c8e295833e684121e024c1a7e1e
remon2929 <ff.remon.9123@gmail.com>
2020-04-24 09:13:54 +0000

    11.22

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-   before_save { self.email = email.downcase }
+attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -20,10 +21,7 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
  end
  
- # ランダムなトークンを返す
-    def new_token
-      SecureRandom.urlsafe_base64
-    end
+
   
   # 永続セッションのためにユーザーをデータベースに記憶する
   def remember
@@ -37,9 +35,8 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
+
+
   
   # ユーザーのログイン情報を破棄する
  def forget
@@ -51,5 +48,18 @@ class User < ApplicationRecord
     session.delete(:user_id)
     @current_user = nil
   end
+  
+   private
+
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
                     
 end
