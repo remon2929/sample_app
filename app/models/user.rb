a4685ddf64c16ded1f764ddebeac95aa3c87b466
remon2929 <ff.remon.9123@gmail.com>
2020-04-27 04:17:35 +0000

    12.15

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,66 +1,77 @@
 class User < ApplicationRecord
-attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
-  validates :name,  presence: true, length: { maximum: 50 }
+
+
+  validates :name,  presence: true, length: { maximum:  50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-               has_secure_password
-               validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-                # 渡された文字列のハッシュ値を返す
-  def self.digest(string)
+  has_secure_password
+  validates :password, presence: true,
+    length: { minimum: 6 }, allow_nil: true
+
+  def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-  
-   # ランダムなトークンを返す
- def User.new_token
-    SecureRandom.urlsafe_base64
- end
- 
 
-  
-  # 永続セッションのためにユーザーをデータベースに記憶する
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+  def User.new_token
+    SecureRandom.urlsafe_base64
   end
 
- def forget
-    update_attribute(:remember_digest, nil)
- end
+  def remember
+    self.remember_token = User.new_token
+    self.update_attribute(:remember_digest,
+      User.digest(remember_token))
+  end
 
-# トークンがダイジェストと一致したらtrueを返す
+  def forget
+    self.update_attribute(:remember_digest, nil)
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
+    digest = self.send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
-  
-def activate
+
+  def activate
     update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
-end
- 
- def send_activation_email
+  end
+
+  def send_activation_email
     UserMailer.account_activation(self).deliver_now
- end
+  end
 
-  
-   private
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
 
-    # メールアドレスをすべて小文字にする
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  private
+
     def downcase_email
-      self.email = email.downcase
+      self.email = self.email.downcase
     end
 
-    # 有効化トークンとダイジェストを作成および代入する
     def create_activation_digest
       self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
+      self.activation_digest = User.digest(self.activation_token)
+      # @user.activation_digest => ハッシュ値
     end
-                    
 end
