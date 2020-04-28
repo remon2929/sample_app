0361ecd366f9e2c81c979089c828b0bceeaad682
remon2929 <ff.remon.9123@gmail.com>
2020-04-28 02:55:44 +0000

    13.35

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,28 +57,22 @@ end
    end
    
   
-  private
+private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
- # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-      store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-    
-     # 正しいユーザーかどうか確認
+
+    # beforeフィルター
+
+    # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
-redirect_to(root_url) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user)
     end
 
-# 管理者かどうか確認
+    # 管理者かどうかを確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
